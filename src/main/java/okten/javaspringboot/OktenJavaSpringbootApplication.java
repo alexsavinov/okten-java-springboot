962f5a6acc8c37fcd93c9e75ad786366789d2baa
alexsavinov <spell477@gmail.com>
2022-12-15 09:43:24 +0200

    Initial commit

---
+++ src/main/java/okten/javaspringboot/OktenJavaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package okten.javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OktenJavaSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OktenJavaSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/okten/javaspringboot/OktenJavaSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package okten.javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktenJavaSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
